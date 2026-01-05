c7d82108d84f4802c7a39daf1510423413719097
Pe7atN <pe7ar.ned@gmail.com>
2026-01-05 22:26:06 +0200

    Setup the kubernetes

---
+++ k8s-stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Shutting down Kubernetes"
+
+echo "Deleting Frontend and Backend..."
+kubectl delete -f k8s/frontend-deployment.yaml --ignore-not-found
+kubectl delete -f k8s/backend-deployment.yaml --ignore-not-found
+
+echo "Deleting Database and Services..."
+kubectl delete -f k8s/db-service.yaml --ignore-not-found
+kubectl delete -f k8s/db-deployment.yaml --ignore-not-found
+
+echo "Deleting Storage..."
+kubectl delete pvc postgres-pvc --ignore-not-found
+
+echo "All resources removed."
