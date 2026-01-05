1ed168e43a957383e339863e9c82cbc4bd02bfbb
Pe7atN <pe7ar.ned@gmail.com>
2026-01-06 00:43:40 +0200

    Fix the k8s integration

--- k8s-stop.sh
+++ k8s-stop.sh
@@ -4,6 +4,7 @@ echo "Shutting down Kubernetes"
 echo "Deleting Frontend and Backend..."
 kubectl delete -f k8s/frontend-deployment.yaml --ignore-not-found
 kubectl delete -f k8s/backend-deployment.yaml --ignore-not-found
+kubectl delete -f k8s/backend-alias.yaml --ignore-not-found
 
 echo "Deleting Database and Services..."
 kubectl delete -f k8s/db-service.yaml --ignore-not-found
